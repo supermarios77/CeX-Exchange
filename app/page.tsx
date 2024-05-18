82a29310584bf5b64d94960bcc769678606dca9b
Mariya B <supermariyabros@gmail.com>
2024-05-18 13:07:19 +0300

    ✨ Add Crypto Price Cards

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,13 @@
-import { Hero } from "@/components/hero";
+import { CryptoPriceCard } from "@/components/Home/cryptoPriceCard";
+import { Hero } from "@/components/Home/hero";
+
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-center justify-between p-5">
       <Hero />
+      <CryptoPriceCard />
     </main>
   );
 }
